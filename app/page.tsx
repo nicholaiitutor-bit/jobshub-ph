e05f270bcb40a5a93d6f7ba739312b1d349dd881
nicholaiitutor-bit <nicholaiitutor@gmail.com>
2026-06-03 15:14:57 +0800

    connect supabase test data

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,15 @@
 import { supabase } from "@/lib/supabaseClient";
 
 export default async function Home() {
-  const { data, error } = await supabase.from("jobs").select("*");
-
-  if (error) {
-    console.log(error);
-  }
+  const { data, error } = await supabase.from("test").select("*");
 
   return (
-    <main style={{ padding: 20 }}>
+    <main>
       <h1>Jobshub PH 🚀</h1>
 
-      {data?.map((job) => (
-        <div key={job.id} style={{ marginBottom: 10 }}>
-          <h3>{job.title}</h3>
-          <p>{job.company} - {job.location}</p>
-          <p>{job.description}</p>
-        </div>
-      ))}
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+
+      {error && <p style={{ color: "red" }}>{error.message}</p>}
     </main>
   );
 }
